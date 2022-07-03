52c8c19a0f8c51a53b9d482bfc95a67e4ac83907
Dzmitry Sasnitski <dmitris3012@gmail.com>
2022-07-03 19:18:16 +0300

    Getting all statement files names

--- pyscripts/csvpriorparse.py
+++ pyscripts/csvpriorparse.py
@@ -1,30 +1,46 @@
 import csv
+import os
 import re
 
-with open('vpsk.csv') as source_csv:
-    source_csv_read = csv.reader(source_csv, delimiter=';')
-    i = 0
-    trunc_source_csv = [
-        ['Операция', 'Сумма', 'Валюта', 'Дата операции по счету',
-         'Комиссия/Money-back',
-         'Обороты по счету', 'Цифровая карта', 'Категория операции', '', 'Дата транзакции', 'Время транзакции']]
-    for row in source_csv_read:
 
-        if not row or len(row) != 10:
-            i += 1
+def getstatementslist():
+    files = os.listdir(os.getcwd())
+    statements_list = []
+    for i in files:
+        statement = re.match('Vpsk_\d{8}.csv', i)
+        if statement is None:
+            pass
         else:
-            if bool(re.search('[а-яА-Я]', row[0])) is False:
-                row[0] = row[0].split(' ')
-                datetime = row[0]
-                row.append(datetime[0])  # extract date from datetime column
-                row.append(datetime[1])  # extract time from datetime column
-                row.remove(row[0])       # remove datetime column
-                # '[0-3][0-9]\.+[0-1][0-9]\.+[0-2][0-9]'
-                row[8] = row[8].replace('\xa0', '')
-                trunc_source_csv.append(row)
-    i += 1
-with open('vpsk_res.csv', 'a') as target_csv:
-    for row in trunc_source_csv:
-        for elem in row:
-            elem = ";".join(elem)
-        target_csv.writelines("%s\n" % str(row))
+            statement = statement.group(0)
+            statements_list.append(statement)
+
+
+def priorstatementparse(filename):
+    with open(filename) as source_csv:
+        source_csv_read = csv.reader(source_csv, delimiter=';')
+        i = 0  # entries iterator
+        trunc_source_csv = [
+            ['Операция', 'Сумма', 'Валюта', 'Дата операции по счету',
+             'Комиссия/Money-back',
+             'Обороты по счету', 'Цифровая карта', 'Категория операции', '', 'Дата транзакции', 'Время транзакции']]
+        for row in source_csv_read:
+
+            if not row or len(row) != 10:
+                i += 1
+            else:
+                if bool(re.search('[а-яА-Я]', row[0])) is False:
+                    row[0] = row[0].split(' ')
+                    datetime = row[0]
+                    row.append(datetime[0])  # extract date from datetime column
+                    row.append(datetime[1])  # extract time from datetime column
+                    row.remove(row[0])  # remove datetime column
+                    row[7] = row[7].replace('\xa0', '')
+                    # 7th element of many rows (after datetime column removal)
+                    # contains unnecessary symbol
+                    trunc_source_csv.append(row)
+        i += 1
+    with open('vpsk_res.csv', 'a') as target_csv:
+        for row in trunc_source_csv:
+            for elem in row:
+                elem = ";".join(elem)
+            target_csv.writelines("%s\n" % str(row))
