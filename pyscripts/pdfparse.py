46eb7835f90b222c17360569618866c0667b9f7d
Dzmitry Sasnitski <dmitris3012@gmail.com>
2022-07-20 22:04:50 +0300

    Proper structure provided

--- pyscripts/pdfparse.py
+++ pyscripts/pdfparse.py
@@ -16,26 +16,14 @@ def getstatementslist():
     return statements_list
 
 
-# def getcardnumber(filename):
-#     with open(filename) as source_csv:
-#         source_csv_read = csv.reader(source_csv, delimiter=';')
-#         pan = ''
-#         for entry in source_csv_read:
-#             if not entry or re.search('Карта:', entry[0]) is None:
-#                 pass
-#             else:
-#                 pan = entry[1][8:12:]
-#                 return pan
-
-
 def mtbstatementparse(filename):
     source_pdf = open(filename, 'rb')
     pdf_read = PyPDF2.PdfReader(source_pdf)
     num_pages = pdf_read.numPages
     page_no = 0
-    # retrieve account number for further purposes (to match with data in destination DB)
+    # retrieve account number, account currency for further purposes (to match with data in destination DB)
     account_no = re.search(r'BY\d{2}MTBK3014\d{16}', pdf_read.getPage(0).extractText()).group(0)
-
+    billing_currency = re.search(r'\w{3}Выписка по сч[е-ё]ту', pdf_read.getPage(0).extractText()).group(0)[:3:]
     # this is what we do for every page of input PDF file
     while page_no < num_pages:
 
@@ -46,73 +34,101 @@ def mtbstatementparse(filename):
         output_page = []
         txn_num = 0
         for transaction in input_page:
+
             transaction = transaction.replace('\n', ' ')
+            # we check if this is debt repayment, if yes - skip
+            if re.search('Погашение.основного.долга', transaction) is not None:
+                txn_num += 1
+            else:
+                # now we check if a real transaction was split within itself as a result of splitting pages into rows
 
-            # now we check if a real transaction was split within itself as a result of splitting pages into rows
+                # Incorrect splitting / last entry handling
+                # if there are non-digits in last 4 - it's either real txn first part OR last txn with service info
+                if re.match(r'\d.\d{2}', transaction[-4::]) is None:
 
-            # Incorrect splitting / last entry handling
-            # if there are non-digits in last 4 - it's either real txn first part OR last txn with service info
-            if re.match(r'\d.\d{2}', transaction[-4::]) is None:
+                    # if this transaction is the last
+                    if re.search(r'\sсумма блокирована', str(transaction)) is not None:
+                        transaction = re.split(r'-\sсумма обработана', transaction)
+                        transaction = ((str(transaction[:1:]).replace("'", '')).replace("[", '')).replace(']', '')
+                    # if this transaction was just split within
+                    else:
+                        transaction = transaction + ' T' + input_page[txn_num + 1].replace('\n', ' ')
 
-                # if this transaction is the last
-                if re.search(r'\sсумма блокирована', str(transaction)) is not None:
-                    transaction = re.split(r'-\sсумма обработана', transaction)
-                    transaction = ((str(transaction[:1:]).replace("'", '')).replace("[", '')).replace(']', '')
-                # if this transaction was just split within
+                    output_page.append(transaction)
+                    txn_num += 1
+
+                # if there are non-digits in first 4 - it's real txn second part
+                elif re.match(r'\d{2}.\d', transaction[:5:]) is None:
+                    txn_num += 1
+
+                # most entries land here
                 else:
-                    transaction = transaction + ' T' + input_page[txn_num + 1].replace('\n', ' ')
-
-                output_page.append(transaction)
-                txn_num += 1
-
-            # if there are non-digits in first 4 - it's real txn second part
-            elif re.match(r'\d{2}.\d', transaction[:5:]) is None:
-                txn_num += 1
-
-            # most entries land here
-            else:
-                output_page.append(transaction)
-                txn_num += 1
-
+                    output_page.append(transaction)
+                    txn_num += 1
         for transaction in output_page:
-            transaction_date = transaction[:10:]
-            transaction_time = transaction[11:19:]
             account_date = transaction[19:29:]
-            card_accno = re.search(r'\d{6}[*]{6}\d{4}', transaction)
-            if card_accno is not None:  # if card number found
-                card_accno = card_accno.group(0)[-4::]
+            transaction_date = (re.search(r'\d{2}.\d{2}.\d{4}\s', transaction).group(0)).strip()
+            transaction = transaction.replace(transaction_date, '')
+            transaction_time = (re.search(r'\s\d{2}:\d{2}:\d{2}', transaction).group(0)).strip()
+            transaction = transaction.replace(transaction_time, '')
+            card_number = re.search(r'\d{6}[*]{6}\d{4}', transaction)
+            if card_number is not None:  # if card number found
+                card_number = card_number.group(0)[-4::]
             else:
-                card_accno = account_no
+                card_number = ' '
             transaction_descr_category = transaction
-
             # description and category extraction
             if re.search(r'Комиссия\s', transaction_descr_category) is None \
-                    and re.search(r'Пополнение сч[а-я]та\s', transaction_descr_category) is None:
+                    and re.search(r'Пополнение сч[а-я]та\s', transaction_descr_category) is None \
+                    and re.search(r'Возврат', transaction_descr_category) is None:
                 # if entry is not a commission and not a ERIP credit then we take it out:
                 # remove dates and amounts, leave only description and category
-                transaction_descr_category = re.split(str(card_accno),
+                transaction_descr_category = re.split(str(card_number),
                                                       re.split(r'BYN\d|USD\d|EUR\d|RUB\d',
                                                                transaction_descr_category)[0])
                 transaction_descr_category = transaction_descr_category[1]
-                # taking out description
-                transaction_descr = re.match(r'(.{1,50}[^а-яА-Я]\s[A-Z]{2}\s){1,30}', transaction_descr_category).group(
-                    0)
+                # taking out description + refund handling
+                if re.match(r'(.{1,50}[^а-яА-Я]\s[A-Z]{2}\s){1,30}', transaction_descr_category) is None:
+                    transaction_descr = ' '
+                else:
+                    transaction_descr = re.match(r'(.{1,50}[^а-яА-Я]\s[A-Z]{2}\s){1,30}', transaction_descr_category) \
+                        .group(0)
                 # removing description, only category left
                 transaction_descr_category = transaction_descr_category.replace(transaction_descr, '')
                 transaction_category = transaction_descr_category
 
-            # if entry is a commission or ERIP credit - fill in with constant data
+            # if entry is a commission or ERIP credit or refund - fill in with constant data
             else:
-                if re.search(r'Комиссия\s', transaction) is None:
+                if re.search(r'Комиссия\s', transaction) is None \
+                        and re.search(r'Возврат', transaction) is None:
                     transaction_descr = 'Пополнение счёта ' + account_no
                     transaction_category = 'Пополнение ЕРИП'
+                elif re.search(r'Возврат', transaction) is not None:
+                    transaction_descr = 'Возврат покупки'
+                    transaction_category = 'Возврат'
                 else:
                     transaction_descr = ' '
                     transaction_category = 'Комиссия по операции'
 
-            csv_output_string = str([transaction_date, transaction_time, account_date, transaction_descr,
-                                     transaction_category, card_accno])
-            print(csv_output_string)
+            # amounts extraction
+            transaction_amount = re.search(r'\w{3}\d{1,5}.\d{2}\s', transaction).group(0)[3::]
+            transaction_currency = re.search(r'\w{3}\d{1,5}.\d{2}\s', transaction).group(0)[:3:]
+            billing_amount = re.search(r'\d{1,5}\.\d{2}[+-]', transaction).group(0)
+            if billing_amount[-1::] == '-':
+                billing_amount = '-' + billing_amount[:-1:]
+            elif billing_amount[-1::] == '+':
+                billing_amount = billing_amount[:-1:]
+            csv_output_string = str([transaction_date,
+                                     transaction_time,
+                                     transaction_descr,
+                                     transaction_amount,
+                                     transaction_currency,
+                                     account_date,
+                                     account_no,
+                                     billing_amount,
+                                     billing_currency,
+                                     card_number,
+                                     transaction_category])
 
             # Writing to a file
             with open('MResult.csv', 'a') as target_csv:
