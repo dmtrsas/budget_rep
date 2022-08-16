ccbc0307b997e97a913e1c9037ad9be90436063e
Dzmitry Sasnitski <dmitris3012@gmail.com>
2022-08-16 22:12:20 +0300

    unnecessary files deleted

--- special/Master.ps1
+++ special/Master.ps1
@@ -1,8 +1,14 @@
-py -3 G:\Budget\DB\prior_to_db\csvpriorparse.py
+#Powershell Master file: 1) launches python statements parsing script; 
+#						 2) executes symbols_term Powershell script; 
+#						 3) deletes original statements from the directory
 
 
-&"G:\Budget\DB\prior_to_db\symbols_term.ps1"
-$FileName = "G:\Budget\DB\prior_to_db\Vpsk*.csv"
-if (Test-Path $FileName) {
-  Remove-Item $FileName
-}
+py -3 G:\Budget\DB\banks_to_db\statements_parse.py
+
+&"G:\Budget\DB\banks_to_db\symbols_term.ps1"
+
+$FileName_Prior = "G:\Budget\DB\banks_to_db\Vpsk*.csv"
+$FileName_MTB = "G:\Budget\DB\banks_to_db\transactions *.pdf"
+
+if (Test-Path $FileName_Prior) {Remove-Item $FileName_Prior} 
+if (Test-Path $FileName_MTB) {Remove-Item $FileName_MTB}
--- special/symbols_term.ps1
+++ special/symbols_term.ps1
@@ -1,26 +1,62 @@
+ #This file is intended to replace symbols which are unnecessary and difficult to process within SSIS
+ #prior file processing
 
- $commadot = get-content -path G:\Budget\DB\prior_to_db\Result.csv
+ $commadot = get-content -path G:\Budget\DB\banks_to_db\Prior_Result.csv
  $notcommadot = $commadot -replace "',","';"
- $notcommadot | set-content -path G:\Budget\DB\prior_to_db\Result.csv
+ $notcommadot | set-content -path G:\Budget\DB\banks_to_db\Prior_Result.csv
  
- $quo = get-content -path G:\Budget\DB\prior_to_db\Result.csv
+ $quo = get-content -path G:\Budget\DB\banks_to_db\Prior_Result.csv
  $notquo = $quo -replace "'"," "
- $notquo | set-content -path G:\Budget\DB\prior_to_db\Result.csv
+ $notquo | set-content -path G:\Budget\DB\banks_to_db\Prior_Result.csv
  
- $spaces = get-content -path G:\Budget\DB\prior_to_db\Result.csv
+ $spaces = get-content -path G:\Budget\DB\banks_to_db\Prior_Result.csv
  $notspaces = $spaces -replace "  "," "
- $notspaces | set-content -path G:\Budget\DB\prior_to_db\Result.csv
+ $notspaces | set-content -path G:\Budget\DB\banks_to_db\Prior_Result.csv
 
- $brackets = get-content -path G:\Budget\DB\prior_to_db\Result.csv
+ $brackets = get-content -path G:\Budget\DB\banks_to_db\Prior_Result.csv
  $firbracket_to_blanks = $brackets -replace '[][]',''
  $secbracket_to_blanks = $brackets -replace '[][]',''
- $firbracket_to_blanks | set-content -path G:\Budget\DB\prior_to_db\Result.csv
- $secbracket_to_blanks | set-content -path G:\Budget\DB\prior_to_db\Result.csv
+ $firbracket_to_blanks | set-content -path G:\Budget\DB\banks_to_db\Prior_Result.csv
+ $secbracket_to_blanks | set-content -path G:\Budget\DB\banks_to_db\Prior_Result.csv
  
- $semicolons_spaces = get-content -path G:\Budget\DB\prior_to_db\Result.csv
+ $semicolons_spaces = get-content -path G:\Budget\DB\banks_to_db\Prior_Result.csv
  $semicolons_spaces_elim = $semicolons_spaces -replace "; ",";" 
- $semicolons_spaces_elim | set-content -path G:\Budget\DB\prior_to_db\Result.csv
+ $semicolons_spaces_elim | set-content -path G:\Budget\DB\banks_to_db\Prior_Result.csv
  
- $semicolons_spaces_2 = get-content -path G:\Budget\DB\prior_to_db\Result.csv
+ $semicolons_spaces_2 = get-content -path G:\Budget\DB\banks_to_db\Prior_Result.csv
  $semicolons_spaces_elim_2 = $semicolons_spaces_2 -replace " ;",";"
- $semicolons_spaces_elim_2 | set-content -path G:\Budget\DB\prior_to_db\Result.csv
+ $semicolons_spaces_elim_2 | set-content -path G:\Budget\DB\banks_to_db\Prior_Result.csv
+ 
+ 
+ #mtb file processing
+ 
+ 
+ $dquo = get-content -path G:\Budget\DB\banks_to_db\MTB_Result.csv
+ $notdquo = $dquo -replace '"',''
+ $notdquo | set-content -path G:\Budget\DB\banks_to_db\MTB_Result.csv
+
+ $commadot = get-content -path G:\Budget\DB\banks_to_db\MTB_Result.csv
+ $notcommadot = $commadot -replace "',","';"
+ $notcommadot | set-content -path G:\Budget\DB\banks_to_db\MTB_Result.csv
+ 
+ $quo = get-content -path G:\Budget\DB\banks_to_db\MTB_Result.csv
+ $notquo = $quo -replace "'"," "
+ $notquo | set-content -path G:\Budget\DB\banks_to_db\MTB_Result.csv
+ 
+ $spaces = get-content -path G:\Budget\DB\banks_to_db\MTB_Result.csv
+ $notspaces = $spaces -replace "  "," "
+ $notspaces | set-content -path G:\Budget\DB\banks_to_db\MTB_Result.csv
+
+ $brackets = get-content -path G:\Budget\DB\banks_to_db\MTB_Result.csv
+ $firbracket_to_blanks = $brackets -replace '[][]',''
+ $secbracket_to_blanks = $brackets -replace '[][]',''
+ $firbracket_to_blanks | set-content -path G:\Budget\DB\banks_to_db\MTB_Result.csv
+ $secbracket_to_blanks | set-content -path G:\Budget\DB\banks_to_db\MTB_Result.csv
+ 
+ $semicolons_spaces = get-content -path G:\Budget\DB\banks_to_db\MTB_Result.csv
+ $semicolons_spaces_elim = $semicolons_spaces -replace "; ",";" 
+ $semicolons_spaces_elim | set-content -path G:\Budget\DB\banks_to_db\MTB_Result.csv
+ 
+ $semicolons_spaces_2 = get-content -path G:\Budget\DB\banks_to_db\MTB_Result.csv
+ $semicolons_spaces_elim_2 = $semicolons_spaces_2 -replace " ;",";"
+ $semicolons_spaces_elim_2 | set-content -path G:\Budget\DB\banks_to_db\MTB_Result.csv
