90213ae499614b88396d6968902d2273824246b2
Dzmitry Sasnitski <106316243+dmtrsas@users.noreply.github.com>
2024-01-13 14:39:46 +0300

    Update delete_statements.ps1

--- special/delete_statements.ps1
+++ special/delete_statements.ps1
@@ -1,5 +1,5 @@
 $FileName_Prior = "G:\Budget\DB\banks_to_db\Vpsk*.csv"
-$FileName_MTB = "G:\Budget\DB\banks_to_db\Выписка_*.pdf"
+$FileName_MTB = "G:\Budget\DB\banks_to_db\Р’С‹РїРёСЃРєР°_*.pdf"
 
 if (Test-Path $FileName_Prior) {Remove-Item $FileName_Prior} 
 if (Test-Path $FileName_MTB) {Remove-Item $FileName_MTB}
